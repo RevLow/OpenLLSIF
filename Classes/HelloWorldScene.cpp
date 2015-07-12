5bbc6a111430e2c78d45cec3045aeff0b583a083
RevLow <rev_low@yahoo.co.jp>
2015-07-13 00:47:54 +0900

    TopSceneの作成

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,4 +1,7 @@
 #include "HelloWorldScene.h"
+#include "ui/cocosGui.h"
+#include "cocostudio/CocoStudio.h"
+#include <SimpleAudioEngine.h>
 
 USING_NS_CC;
 
@@ -26,6 +29,41 @@ bool HelloWorld::init()
     {
         return false;
     }
+    
+    //////////////////////////////
+    //2, シーンの読み込み
+    //
+    
+    FileUtils::getInstance()->addSearchPath("res");
+    auto topScene = CSLoader::getInstance()->createNode("res/MainScene.csb");
+    
+    auto view = Director::getInstance()->getOpenGLView();
+    auto frame = view->getFrameSize();
+    
+    //Retinaじゃない場合
+    if(frame.width <= 480)
+    {
+        //topScene->setAnchorPoint(Vec2(0.0,1.0));
+        topScene->setPosition(0, 0);
+        topScene->setScale(0.5);
+    }
+
+    addChild(topScene);
+    
+    
+    //////////////////////////////
+    //3, BGM再生
+    //
+    CocosDenshion::SimpleAudioEngine::getInstance()->playBackgroundMusic("Sound/BGM/paradice_live.mp3");
+    
+    //////////////////////////////
+    //4, アニメーション読み込み
+    //
+    auto action = cocostudio::timeline::ActionTimelineCache::getInstance()->createAction("res/MainScene.csb");
+    auto textLabel = topScene->getChildByName<Label*>("Text_1");
+    
+    textLabel->runAction(action);
+    action->gotoFrameAndPlay(0, true);
     
     return true;
 }
